a74c9a00216aa59e1afc4997604818f6f3159ed3
Michael Chan <michaelcychan@gmail.com>
2022-12-06 21:49:23 +0000

    implement $ to cash balance, refactor test assert function

--- account.go
+++ account.go
@@ -1,17 +1,25 @@
 package account
 
+import "fmt"
+
+type Cash int
+
 type Account struct {
-	cashBalance int
+	cashBalance Cash
 }
 
-func (a *Account) ShowCashBalance() int {
+func (a *Account) ShowCashBalance() Cash {
 	return a.cashBalance
 }
 
-func (a *Account) DepositCash(cashAmount int) {
+func (a *Account) DepositCash(cashAmount Cash) {
 	a.cashBalance += cashAmount
 }
 
-func (a *Account) WithdrawCash(cashAmount int) {
+func (a *Account) WithdrawCash(cashAmount Cash) {
 	a.cashBalance -= cashAmount
+}
+
+func (c Cash) String() string {
+	return fmt.Sprintf("$ %d", c)
 }
--- account_test.go
+++ account_test.go
@@ -6,35 +6,35 @@ func TestAccount(t *testing.T) {
 	t.Run("returns 0 as initial cash account balance", func(t *testing.T) {
 		account := Account{}
 
-		expected := 0
+		expected := Cash(0)
 		actual := account.ShowCashBalance()
 
-		if actual != expected {
-			t.Errorf("Expected account balance to be %d, but got %d", expected, actual)
-		}
+		assertCashBalance(t, expected, actual)
 	})
-	t.Run("deposits would not create error and increase balance", func(t *testing.T) {
+	t.Run("deposits Cash would not create error and increase balance", func(t *testing.T) {
 		account := Account{}
 		account.DepositCash(10)
 
-		expected := 10
+		expected := Cash(10)
 		actual := account.ShowCashBalance()
 
-		if actual != expected {
-			t.Errorf("Expected account balance to be %d, but got %d", expected, actual)
-		}
+		assertCashBalance(t, expected, actual)
 	})
-	t.Run("Withdraw an amount less than balance creates no error and decrease balance accordingly", func(t *testing.T) {
+	t.Run("Withdraws a Cash amount less than balance creates no error and decrease balance accordingly", func(t *testing.T) {
 		account := Account{}
-		account.DepositCash(10)
-		account.WithdrawCash(7)
+		account.DepositCash(Cash(10))
+		account.WithdrawCash(Cash(7))
 
-		expected := 3
+		expected := Cash(2)
 		actual := account.ShowCashBalance()
 
-		if actual != expected {
-			t.Errorf("Expected account balance to be %d, but got %d", expected, actual)
-		}
+		assertCashBalance(t, expected, actual)
 	})
+}
 
+func assertCashBalance(t testing.TB, expected, actual Cash) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("Expected Cash account balance to be %s, but got %s", expected, actual)
+	}
 }
